89e221518745c84f4efea50a9961976258916690
soliu <solixzsystem@gmail.com>
2015-05-30 10:41:06 +0100

    Beginning to customize the HTML template.

--- DjangoGirl/urls.py
+++ DjangoGirl/urls.py
@@ -5,7 +5,7 @@ Definition of urls for DjangoGirl.
 from datetime import datetime
 from django.conf.urls import patterns, url
 from app.forms import BootstrapAuthenticationForm
-
+#import blog
 # Uncomment the next lines to enable the admin:
 from django.conf.urls import include
 from django.contrib import admin
@@ -13,31 +13,32 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    url(r'^$', 'blog.views.home', name='home'),
-    url(r'^contact$', 'app.views.contact', name='contact'),
-    url(r'^about', 'app.views.about', name='about'),
-    url(r'^login/$',
-        'django.contrib.auth.views.login',
-        {
-            'template_name': 'app/login.html',
-            'authentication_form': BootstrapAuthenticationForm,
-            'extra_context':
-            {
-                'title':'Log in',
-                'year':datetime.now().year,
-            }
-        },
-        name='login'),
-    url(r'^logout$',
-        'django.contrib.auth.views.logout',
-        {
-            'next_page': '/',
-        },
-        name='logout'),
+    #url(r'^$', 'blog.views.home', name='home'),
+    #url(r'^contact$', 'app.views.contact', name='contact'),
+    #url(r'^about', 'app.views.about', name='about'),
+    #url(r'^login/$',
+    #    'django.contrib.auth.views.login',
+    #    {
+    #        'template_name': 'app/login.html',
+    #        'authentication_form': BootstrapAuthenticationForm,
+    #        'extra_context':
+    #        {
+    #            'title':'Log in',
+    #            'year':datetime.now().year,
+    #        }
+    #    },
+    #    name='login'),
+    #url(r'^logout$',
+    #    'django.contrib.auth.views.logout',
+    #    {
+    #        'next_page': '/',
+    #    },
+    #    name='logout'),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
+     url(r'',include('blog.urls')),   
      url(r'^admin/', include(admin.site.urls)),
 )
--- blog/views.py
+++ blog/views.py
@@ -1,17 +1,4 @@
-from django.shortcuts import render
-from django.http import HttpRequest
-from django.template import RequestContext
-from datetime import datetime
+from django.shortcuts import redirect, render
 
-def home(request):
-    """Renders the home page."""
-    assert isinstance(request, HttpRequest)
-    return render(
-        request,
-        'index.html',
-        context_instance = RequestContext(request,
-        {
-            'title':'Home Page',
-            'year':datetime.now().year,
-        })
-    )
+def post_list(request):
+    return render(request,"index.html",{})
