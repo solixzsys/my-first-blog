c98c0da9c6104e22696124ea941d1169fe5416ee
soliu <solixzsystem@gmail.com>
2015-05-30 17:53:24 +0100

    detail page creation

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import redirect, render
+from django.shortcuts import redirect, render, get_object_or_404
 from blog.models import Post
 from django.utils import timezone
 def post_list(request):
     posts=Post.objects.filter(published_date__lt=timezone.now()).order_by("published_date")
     return render(request,"index.html",{ 'posts': posts })
+
+def post_detail(request,pk):
+    post=get_object_or_404(Post,pk=pk)
+    return render(request,"detail.html",{ 'post': post })
