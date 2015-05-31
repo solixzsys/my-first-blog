73ddb9948cadd699d337e6157733cd3b3411c0ea
soliu <solixzsystem@gmail.com>
2015-05-31 18:52:39 +0100

    Addition of publishing, removing of post

--- blog/views.py
+++ blog/views.py
@@ -3,15 +3,15 @@ from blog.models import Post
 from django.utils import timezone
 from blog.forms import PostForm
 def post_list(request):
-    posts=Post.objects.filter(published_date__lt=timezone.now()).order_by("published_date")
+    posts=Post.objects.filter(published_date__lt=timezone.now()).order_by("-published_date")
     return render(request,"index.html",{ 'posts': posts })
 
 def post_detail(request,pk):
     post=get_object_or_404(Post,pk=pk)
-    if post.published_date is not None:
-        return render(request,"detail.html",{ 'post': post })
-    else:
-        return redirect('blog.views.post_list')
+    #if post.published_date is not None:
+    return render(request,"detail.html",{ 'post': post })
+    #else:
+    #    return redirect('blog.views.post_list')
 
 
 def post_new(request):
@@ -43,3 +43,19 @@ def post_edit(request,pk):
     else:
         form=PostForm(instance=post)
         return render(request,'post_edit.html',{'form':form})
+
+
+def post_draft(request):
+    posts=Post.objects.filter(published_date__isnull=True).order_by('created_date')
+    return render(request,"index.html",{ 'posts': posts })
+
+
+def post_publish(request,pk):
+    post=get_object_or_404(Post,pk=pk)
+    post.publish()
+    return render(request,"detail.html",{ 'post': post })
+
+def post_remove(request,pk):
+    post=get_object_or_404(Post,pk=pk)
+    post.delete()
+    return redirect("/")
